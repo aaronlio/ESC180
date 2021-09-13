7316b8a44fc2d4eaf8fb5aa9906c2a0088fabbba
Aaron Lio <bossaaron3@MacBook-Air.local>
2021-09-13 19:19:21 -0400

    lab 1 commit

---
+++ hello.py
@@ -0,0 +1,31 @@
+# Problem 1
+print("Hello, Python")
+
+# Problem 2
+greeting = "Hello, "
+name1 = "Aaron Lio"
+name2 = "Jae Gwan Park"
+print(greeting + name1)
+print(greeting + name2)
+
+# Problem 3
+print(greeting + name1 + " and " + name2 + ". Your names are " + name1 + " and " + name2 + ". Hi there. Your names are still " + name1 + " and " + name2 + ".")
+print(f"{greeting}{name1} and {name2}. Your names are {name1} and {name2}. Hi there. Your names are still {name1} and {name2}.")
+
+# Problem 4
+name1 = "Prof. Cluett"
+name2 = "Prof. Thywissen"
+
+
+# Problem 5 
+greetee = "Lord Voldemort"
+
+if greetee == "voldemort":
+    print("I'm not talking to you")
+else:
+    print(greeting + greetee)
+
+
+
+
+
