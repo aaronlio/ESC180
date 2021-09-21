8f2e157b74d1f5597bacf64cb69d1de5102fb126
Aaron Lio <bossaaron3@MacBook-Air-5.local>
2021-09-21 12:15:19 -0400

    Pre Lecture 6 Commit

---
+++ Lecture6.py
@@ -0,0 +1,12 @@
+#<----------------------------------- Lecture 6 ----------------------------------->
+#<------------- For Loops -------------->
+
+"Used to repeat a block a number of times, and do something each time it repeats"
+
+for i in range(5):
+    print("I <3 EngSci")
+
+for i in range(5):
+    print( 2 * i )
+    print(i)
+    print("==========")
